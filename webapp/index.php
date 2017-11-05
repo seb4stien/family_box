395218de67df53abc2bd2cb15e219f5f820ace93
Sebastien <seb4stien@georget.name>
2017-11-05 22:30:05 +0100

    Start rebranding

--- webapp/index.php
+++ webapp/index.php
@@ -1,33 +1,26 @@
 <!doctype html>
 <html lang="en">
 	<head>
-		<meta name="mobile-web-app-capable" content="yes">
-
 		<title>Regarder une vidéo</title>
-		<!-- Required meta tags -->
+
+		<meta name="mobile-web-app-capable" content="yes">
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
 		<link rel="manifest" href="manifest.json">
 
-		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="vendor/bootstrap-4.0.0/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 
 		<script src="vendor/angular.min.js"></script>
 		<script src="js/player.js"></script>
 	</head>
 	<body ng-app="playerApp">
-		<div class="container" ng-controller="playerController">
-			<div class="row">
-				<div class="col-sm">
-					<h2>Regarder une vidéo</h2>
-				</div>
-				<div class="col-sm text-right">
-				</div>
-			</div>
-
-			<br />
-
+		<div class="container-fluid" ng-controller="playerController" style="border: 1px solid black; padding: 0px">
+			<nav class="navbar navbar-dark bg-dark">
+				<b><a class="navbar-brand" href="/">Contrôle de la TV</a></b>
+				<ul class="navbar-nav">
+					<li class="nav-item"><a class="nav-link" href="#">aide</a></li>
+				</ul>
+			</nav>
 
 			<div class="row" style="height: 400px">
 				<div class="col-sm">
