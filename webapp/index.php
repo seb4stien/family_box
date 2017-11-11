32210c8b80779f22a731106cfda4ef3498477bc9
Sebastien <seb4stien@georget.name>
2017-11-11 18:14:51 +0100

    Indent

--- webapp/index.php
+++ webapp/index.php
@@ -23,9 +23,27 @@
 				</ul>
 			</nav>
 
-			<ng-view></ng-view>
-
-		</div>
+			<div class="row">
+				<div class="col">
+					<ul class="nav flex-column">
+						<li class="nav-item">
+							<a class="nav-link active" href="#">Active</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Link</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Link</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link disabled" href="#">Disabled</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col">
+					<ng-view></ng-view>
+				</div>
+			</div>
 
 	</body>
 </html>
